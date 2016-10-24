2eb93e777fecb60df2f3a4a73b7781e4648fba09
Blake_Strebel <blakestrebel2018@u.northwestern.edu>
2016-10-23 19:31:31 -0500

    created node to control joint states

---
+++ src/robot_control_node.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+import rospy
+from math import cos, sin, pi
+
+from sensor_msgs.msg import JointState
+
+def control():
+    pub = rospy.Publisher('control_joint_states',JointState,queue_size=10)
+    rate = rospy.Rate(50)
+    joint_state = JointState()
+
+    while not rospy.is_shutdown():
+        t = rospy.Time.now().to_sec()
+
+        joint_state.header.stamp = rospy.Time.now()
+        joint_state.name = ["base_to_link1","link1_to_link2"]
+        joint_state.position = [1, 1]
+        pub.publish(joint_state)
+
+        rate.sleep()
+
+if __name__ == '__main__':
+    rospy.init_node('robot_control_node')
+    try:
+        control()
+    except rospy.ROSInterruptException:
+        pass
+
+rospy.spin()
